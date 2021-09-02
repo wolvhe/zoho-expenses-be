75d5294cca7b59d67085494a3dd4063ad6396cde
pavi <pavithirandxd@gmail.com>
2021-09-02 18:08:32 +0530

    changes

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+
+var routes=require('./routes/index.js');
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+var cors = require('cors');
+var dotenv=require('dotenv');
+var mongoose=require('mongoose');
+
+
+dotenv.config();
+app.use(cors());
+
+
+
+mongoose.connect(process.env.db_con,{ useNewUrlParser: true,useUnifiedTopology: true },()=>{
+   console.log("db connected....");
+})
+
+
+
+app.use('/api',routes);
+
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+
+app.listen(3000,()=>{
+    console.log("App listeing on 3000");
+});
