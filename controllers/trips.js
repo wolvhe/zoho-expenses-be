09667a59e3289de7fa0c659787f9fea33c688af4
Arul1541 <arulkumarans1504@gmail.com>
2021-09-06 11:20:42 +0530

    trips-be

--- controllers/trips.js
+++ controllers/trips.js
@@ -2,16 +2,20 @@ const Trip= require('../models/trips');
 
 const savetrip= async(req,res)=>{
     const trip=new Trip({
+        // trip_id:_id,
         travel_type:req.body.travel_type,
         trip_name:req.body.trip_name,
-        business_purpose:req.body.business_purpose
+        trip_destination:req.body.trip_destination,
+        trip_visa:req.body.trip_visa,
+        business_purpose:req.body.business_purpose,
+        mode_of_transport:req.body.mode_of_transport
     })
 
     console.log(trip);
     try {
         
-        const savedUser = await cust.save();
-        console.log("saved");
+        const Savetrip = await trip.save();
+        console.log(Savetrip);
         return res.send("sucessfully saved");
     } catch (err) {
         return res.status(400).json(err);
--- models/trips.js
+++ models/trips.js
@@ -2,11 +2,11 @@ var mongoose=require('mongoose');
 const { boolean } = require('webidl-conversions');
 
 const tripSchema=new mongoose.Schema({
-    // trip_id:{
-    //     type:Number,
-    //     min:6,
-    //     max:255
-    // },
+    trip_id:{
+        type:Number,
+        min:6,
+        max:255
+    },
     travel_type:{
         type:String,
         min:6,
@@ -17,37 +17,125 @@ const tripSchema=new mongoose.Schema({
         min:6,
         max:255
     },
-    // trip_designation:{
-    //     type:String,
-    //     min:6,
-    //     max:255
-    // },
-    // trip_visa:{
-    //     type:Boolean,
-    //     min:6,
-    //     max:255
-    // },
+    trip_designation:{
+        type:String,
+        min:6,
+        max:255
+    },
+    trip_visa:{
+        type:Boolean,
+        min:6,
+        max:255
+    },
     business_purpose:{
         type:String,
         min:6,
         max:255
     },
+    mode_of_transport:{
+        type:String
+    },
 
-    // flight_type:{
-    //     type:String,
-    //     min:6,
-    //     max:255
-    // },
-    // flight_depart_from:{
-    //     type:String,
-    //     min:6,
-    //     max:255
-    // },
-    // flight_description:{
-    //     type:String,
-    //     min:6,
-    //     max:255
-    // }
+    flight_type:{
+        type:String,
+        min:6,
+        max:255
+    },
+    flight_depart_from:{
+        type:String,
+        min:6,
+        max:255
+    },
+    flight_arrive_at:{
+        type:String,
+        min:6,
+        max:255
+    },
+    flight_departure_time:{
+        type:String,
+        min:6,
+        max:255
+    },
+    flight_description:{
+        type:String,
+        min:6,
+        max:255
+    },
+    hotel_location:{
+        type:String,
+        min:6,
+        max:255
+    },
+    hotel_check_in:{
+        type:String,
+        min:6,
+        max:255
+    },
+    hotel_check_out:{
+        type:String,
+        min:6,
+        max:255
+    },
+    hotel_description:{
+        type:String,
+        min:6,
+        max:255
+    },
+    car_pick_up:{
+        type:String,
+        min:6,
+        max:255
+    },
+    car_drop_off:{
+        type:String,
+        min:6,
+        max:255
+    },
+    car_description:{
+        type:String,
+        min:6,
+        max:255
+    },
+    bus_depart_from:{
+        type:String,
+        min:6,
+        max:255
+    },
+    bus_arrive_at:{
+        type:String,
+        min:6,
+        max:255
+    },
+    bus_departure_time:{
+        type:String,
+        min:6,
+        max:255
+    },
+    bus_description:{
+        type:String,
+        min:6,
+        max:255
+    },
+    train_depart_from:{
+        type:String,
+        min:6,
+        max:255
+    },
+    train_arrive_at:{
+        type:String,
+        min:6,
+        max:255
+    },
+    train_departure_time:{
+        type:String,
+        min:6,
+        max:255
+    },
+    train_description:{
+        type:String,
+        min:6,
+        max:255
+    },
 
 });
 
