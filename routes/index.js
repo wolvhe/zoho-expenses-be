a06900bbb7f63faab2c1cc64bac96bb737097878
RanjithSiva22 <ranjithsiva2211@gmail.com>
2021-09-15 14:30:25 +0530

    expense and bulk expense backend

--- routes/index.js
+++ routes/index.js
@@ -1,46 +1,107 @@
-var express=require('express');
-var router=express.Router();
+var express = require('express');
+var router = express.Router();
 
-const {signup}=require('../controllers/signup')
-const {login}=require('../controllers/login')
-const {savetrip}=require('../controllers/trips')
-const {saveadvance}=require('../controllers/advance')
-const Cust= require('../models/Cust')
-const Trip= require('../models/trips');
-var tokenauth=require('../controllers/verifyToken');
+const { signup } = require('../controllers/signup')
+const { login } = require('../controllers/login')
+const { savetrip } = require('../controllers/trips')
+const { saveadvance } = require('../controllers/advance')
+const { expense } = require('../controllers/expense')
+const { bulkexpenses } = require('../controllers/bulkexpenses')
+const { report } = require('../controllers/report')
 
 
-router.get('/getname/:mail',async (req,res)=>{
+
+
+const Cust = require('../models/Cust')
+const Trip = require('../models/trips');
+const Report = require('../models/report');
+
+
+var tokenauth = require('../controllers/verifyToken');
+const Expense = require('../models/expense');
+
+
+// router.post('/sortexpenses', async (req, res) => {
+//     console.log(req.body);
+//     const userinfo = await Expense.findOne({ email: req.body.email });
+//     console.log(userinfo.expenses);
+//     const arr = userinfo.expenses;
+//     const sorted=arr.filter(ex=>ex.category===req.body.category)
+//     res.send(sorted);
+// });
+
+router.post('/sortexpenses', async (req, res) => {
+    console.log(req.body);
+    let a=1;
+    const b="expenses."+req.body.field;
+    if(req.body.order!=="asc"){
+        a=-1;
+    }
+    Expense.aggregate([
+        // Initial document match (uses index, if a suitable one is available)
+        { $match: {
+            email:req.body.email
+        }},
+    
+        // Expand the scores array into a stream of documents
+        { $unwind: '$expenses' },
+    
+        // Sort in descending order
+        { $sort: {
+            [b] : a
+        }}
+    ]).then(itm=>res.send(itm))
+    .catch(err=>{
+        console.log(err);
+        res.send(err);
+    })
+    // res.send(sorted);
+});
+
+
+router.get('/getname/:mail', async (req, res) => {
     // console.log(req.params);
-    const userinfo= await Cust.findOne({email:req.params.mail});
+    const userinfo = await Cust.findOne({ email: req.params.mail });
     // console.log(userinfo);
 
     res.send(userinfo.org_name);
 });
 
-router.get('/gettrip/:mail',async (req,res)=>{
+router.get('/gettrip/:mail', async (req, res) => {
     console.log(req.params);
-    const tripinfo= await Trip.findOne({email:req.params.mail});
+    const tripinfo = await Trip.findOne({ email: req.params.mail });
     console.log(tripinfo);
 
     res.send(tripinfo);
 });
 
-router.get('/test',(req,res)=>{
+router.get('/getreports/:mail', async (req, res) => {
+    // console.log(req.params);
+    const repinfo = await Report.findOne({ email: req.params.mail });
+    // console.log(repinfo);
+    const repname=[];
+    repinfo.reports.map(i=>repname.push(i.rep_name))
+    res.send(repname);
+});
+
+router.get('/test', (req, res) => {
     console.log("hello");
     res.send("working");
 });
-router.get('/getuser',(req,res)=>{
-    console.log("hibjgfyd");
-    res.send("working");
-})
+
 
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/trip', savetrip);
-router.post('/advance',saveadvance);
+router.post('/advance', saveadvance);
+router.post('/newexpense', expense);
+router.post('/bulkexpenses', bulkexpenses);
+router.post('/addreport', report);
 
 
 
-module.exports=router;
+
+
+
+module.exports = router;
